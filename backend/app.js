82d0d0f7f79ea39b8e054491b81c783bf965cdfb
Rohit Singh <rohitsingh66604@gmail.com>
2023-04-30 16:46:26 +0530

    /

--- backend/app.js
+++ backend/app.js
@@ -22,12 +22,10 @@ app.use(cors());
 const product = require('./routes/productRoute')
 const user = require('./routes/userRoute')
 const order = require('./routes/orderRoute')
-const payment = require('./routes/paymentRoute')
 
 app.use('/api/v1',product);
 app.use('/api/v1',user);
 app.use('/api/v1',order);
-app.use('/api/v1',payment);
 
 app.use(express.static(path.join(__dirname,"../frontend/build")))
 
